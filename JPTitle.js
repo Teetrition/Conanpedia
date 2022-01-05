be6af61dae3de902404d592563d2a6d236ebdcdd
Teetrition <pkj20000716@live.com>
2022-01-05 15:07:56 +0800

    Update JPTitle.js

--- JPTitle.js
+++ JPTitle.js
@@ -1,8 +1,6 @@
 var pageTitle = document.querySelector('h1').innerHTML;
-if (document.getElementById('JPTitle') !== null) {
-    document.querySelector('h1').setAttribute('lang', 'ja');
-    pageTitle = pageTitle.replace('的版本间的差异', '<span lang="zh">的版本间的差异</span>');
-    pageTitle = pageTitle.replace('编辑', '<span lang="zh">编辑</span>');
-    pageTitle = pageTitle.replace('正在创建', '<span lang="zh">正在创建</span>');
-}
+pageTitle = pageTitle.replace('的版本间的差异', '<span lang="zh">的版本间的差异</span>');
+pageTitle = pageTitle.replace('编辑', '<span lang="zh">编辑</span>');
+pageTitle = pageTitle.replace('正在创建', '<span lang="zh">正在创建</span>');
 document.querySelector('h1').innerHTML = pageTitle;
+document.querySelector('h1').setAttribute('lang', 'ja');
